653e3770d49d6c41955818b07ce2a26998763f7e
duchoangitt <Duchoangitt@gmail.com>
2019-09-19 14:08:48 +0700

    commit

--- library/src/main/java/com/hoangit/library/ContactManager.java
+++ library/src/main/java/com/hoangit/library/ContactManager.java
@@ -31,6 +31,7 @@ public class ContactManager {
         return getContactCursor(context,partialName, Contact.Sort.Alphabetical);
     }
 
+
     public Contact.ContactCursor getContactCursor(Context context,String partialName, Contact.Sort sortOrder) {
         String clause = String.format("%s = 1",
                 ContactsContract.PhoneLookup.HAS_PHONE_NUMBER);
@@ -39,6 +40,29 @@ public class ContactManager {
         Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, partialName);
         return new Contact.ContactCursor(contentResolver.query(uri, null, clause, null, sortOrder.getKey()));
     }
+
+    public Contact.ContactCursor getContactCursorFromNumber(Context context,String number) {
+        return getContactCursor(context,number, Contact.Sort.Alphabetical);
+    }
+
+    public Contact.ContactCursor getContactCursorFromNumber(Context context,String number, Contact.Sort sortOrder) {
+        String[] mProjection = { ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.NUMBER, ContactsContract.PhoneLookup.DISPLAY_NAME };
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, number);
+        return new Contact.ContactCursor(contentResolver.query(uri, mProjection, null, null, sortOrder.getKey()));
+    }
+
+    public Contact.ContactCursor getContactCursorFromId(Context context,String id) {
+        return getContactCursor(context,id, Contact.Sort.Alphabetical);
+    }
+
+    public Contact.ContactCursor getContactCursorFromId(Context context,String id, Contact.Sort sortOrder) {
+        String[] mProjection = { ContactsContract.PhoneLookup._ID};
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, id);
+        return new Contact.ContactCursor(contentResolver.query(uri, mProjection, null, null, sortOrder.getKey()));
+    }
+
 
     public boolean contactExists(Context context, String number) {
         Uri lookupUri = Uri.withAppendedPath(
